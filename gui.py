3e7b9d179bcec207d7ffc4526da149b082562177
jrsussner <jrsussner@gmail.com>
2024-08-11 17:07:31 -0500

    Update gui.py
    Added logic so that the submit button's command will run a function to grab the user_input

--- gui.py
+++ gui.py
@@ -3,12 +3,20 @@ import csv
 
 root = Tk()
 
+
+# function to grab user input from entry bar convert it to an integer and make it global
+# "week" can now be changed out with "row" in the function "labels_buttons"
+def userInput():
+    global week
+    week = user_input.get()
+
+
 def labels_buttons(root, csv_file):
     with open(csv_file, "r", encoding="utf-8") as file:
         read = csv.reader(file)
         next(read)
         for row, index in enumerate(read):
-            #Need help here, I have row set to 0 but this is where I would like to generate user input
+            # Need help here, I have row set to 0 but this is where I would like to generate user input
             if row == 0:
                 ravens = Label(root, text=index[0]).grid(row=1, column=0, sticky=W)
                 cheifs = Label(root, text=index[1]).grid(row=2, column=0, sticky=W)
@@ -20,9 +28,12 @@ def labels_buttons(root, csv_file):
                 ml_home = Button(root, text=index[7]).grid(row=2, column=3)
 
 
-question=Label(root, text="What game do you want to bet on?").grid(row=10, column=0, sticky=W)
-user_input = Entry(root, width=25).grid(row=10, column=2, sticky=W)
-submit = Button(root, text="Submit").grid(row=10, column=3)
+question = Label(root, text="What game do you want to bet on?").grid(
+    row=10, column=0, sticky=W
+)
+user_input = Entry(root, width=25)
+user_input.grid(row=10, column=2, sticky=W)
+submit = Button(root, text="Submit").grid(row=10, column=3, command=userInput())
 
 teams_title = Label(root, text="Teams").grid(row=0, column=0, sticky=W)
 spread_title = Label(root, text="Spread", width=10).grid(row=0, column=1)
@@ -30,7 +41,6 @@ spread_odds_title = Label(root, text="Spread odds", width=10).grid(row=0, column
 ml_title = Label(root, text="Money line", width=10).grid(row=0, column=3)
 
 
-
-labels_buttons(root, 'lines.csv')
+labels_buttons(root, "lines.csv")
 
 root.mainloop()
