a33a39960702dc9e037b7a5f05a9e7aab0ea0abe
jrsussner <jrsussner@gmail.com>
2024-08-11 22:10:43 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -41,7 +41,7 @@ def labels_buttons(root, csv_file):
 
 
 scrollbar = Scrollbar(root)
-scrollbar.grid(row=0, column=4, rowspan=11, sticky="ns")
+scrollbar.pack(side=RIGHT, fill=Y)
 
 # Initialize Labels
 question = Label(root, text="What week do you want to bet on?")
