ac146ce7ba79ef1df8b4c1417bd09359e8a6920f
jrsussner <jrsussner@gmail.com>
2024-08-11 21:04:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -20,6 +20,12 @@ moneyline = soup.find_all(
     "span", class_="sportsbook-odds american no-margin default-color"
 )
 
+## CHANGE ##
+randos = soup.find_all("th", class_="always-left column-header")
+for rando in randos:
+    print(rando.text)
+## CHANGE##
+
 # store names and odds into variables for same
 ravens = teams[0]
 ravensSpread = spreads[0]
@@ -57,20 +63,31 @@ with open("lines.txt", "w", newline="", encoding="utf-8") as txtfile:
 with open("lines.csv", "w", newline="", encoding="utf-8") as csvfile:
     csvWriter = csv.writer(csvfile)
     # Writing the labels for each column
-    csvWriter.writerow(["Away Team","Home Team","Away Team Spread", "Away Team Spread Odds", "Away Team ML", "Home Team Spread", "Home Team Spread Odds", "Home Team ML"])
-    gameCounter=0
-    while gameCounter<len(teams):
+    csvWriter.writerow(
+        [
+            "Away Team",
+            "Home Team",
+            "Away Team Spread",
+            "Away Team Spread Odds",
+            "Away Team ML",
+            "Home Team Spread",
+            "Home Team Spread Odds",
+            "Home Team ML",
+        ]
+    )
+    gameCounter = 0
+    while gameCounter < len(teams):
         # exampleList[gameCounter] is the example List of the away team and exampleList[gameCounter+1] is the home team
-        csvWriter.writerow([
-            teams[gameCounter].text,
-            teams[gameCounter + 1].text,
-            spreads[gameCounter].text,
-            spreadOdds[gameCounter].text,
-            moneyline[gameCounter].text,
-            spreads[gameCounter + 1].text,
-            spreadOdds[gameCounter + 1].text,
-            moneyline[gameCounter + 1].text
-        ])
-        gameCounter+=2  # Incrementing by 2 to correctly index the next game
-
-
+        csvWriter.writerow(
+            [
+                teams[gameCounter].text,
+                teams[gameCounter + 1].text,
+                spreads[gameCounter].text,
+                spreadOdds[gameCounter].text,
+                moneyline[gameCounter].text,
+                spreads[gameCounter + 1].text,
+                spreadOdds[gameCounter + 1].text,
+                moneyline[gameCounter + 1].text,
+            ]
+        )
+        gameCounter += 2  # Incrementing by 2 to correctly index the next game
