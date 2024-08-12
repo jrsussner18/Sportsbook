3775547e037b9d1f1579b861b0e11043c39617cf
Matthew Gathman <matthewdgathman@gmail.com>
2024-08-12 15:51:00 -0500

    Small GUI changes to make it look better

--- gui.py
+++ gui.py
@@ -25,21 +25,20 @@ def calcOdds(odds):
 
 widgets = []
 
-
+# Function that changes the given week, clears any previous widgets, and adds new ones
 def change(selection):
     global test
     test = selection
     clear_widgets()
     labels_buttons(root, "lines.csv")
 
-
+# Function that deletes all widgets from the list
 def clear_widgets():
-    """Clear the widgets from the window."""
     for widget in widgets:
         widget.pack_forget()
     widgets.clear()
 
-
+# List of different weeks to choose from for the option Menu
 options = [
     "Week 1",
     "Week 2",
@@ -72,21 +71,37 @@ def labels_buttons(root, csv_file):
     with open(csv_file, "r", encoding="utf-8") as file:
         read = csv.reader(file)
         next(read)
+        # Add Labels and then packing and appending them to a list(widgets) so that they can be deleted later
+        titleFrame=Frame(root)
+        teams_title = Label(titleFrame, text="Teams",width=15)
+        spread_title = Label(titleFrame, text="Spread", width=10)
+        spread_odds_title = Label(titleFrame, text="Spread odds", width=10)
+        ml_title = Label(titleFrame, text="Money line", width=10)
+        titleFrame.pack()
+        teams_title.pack(side=LEFT)
+        spread_title.pack(side=LEFT)
+        spread_odds_title.pack(side=LEFT)
+        ml_title.pack(side=LEFT)
+        widgets.append(titleFrame)
+        widgets.append(teams_title)
+        widgets.append(spread_title)
+        widgets.append(spread_odds_title)
+        widgets.append(ml_title)
         for x, index in enumerate(read):
-            # Need help here, I have row set to 0 but this is where I would like to generate user input
             if index[-1] == test.split(" ")[1]:
                 # Initialize Labels
                 away_frame = Frame(root)
                 home_frame = Frame(root)
-                away = Label(away_frame, text=index[0])
-                home = Label(home_frame, text=index[1])
-                spread_away = Label(away_frame, text=index[2], fg="red", bg="blue")
+                away = Label(away_frame, text=index[0],width=15)
+                home = Label(home_frame, text=index[1],width=15)
+                spread_away = Label(away_frame, text=index[2], fg="red", bg="blue",width=10)
                 spread_odds_away = Button(
                     away_frame,
                     text=index[3],
                     fg="red",
                     bg="blue",
                     command=lambda odds=index[3]: calcOdds(odds),
+                    width=10
                 )
                 ml_away = Button(
                     away_frame,
@@ -94,14 +109,16 @@ def labels_buttons(root, csv_file):
                     fg="red",
                     bg="blue",
                     command=lambda odds=index[4]: calcOdds(odds),
+                    width=10
                 )
-                spread_home = Label(home_frame, text=index[5], fg="blue", bg="red")
+                spread_home = Label(home_frame, text=index[5], fg="blue", bg="red",width=10)
                 spread_odds_home = Button(
                     home_frame,
                     text=index[6],
                     fg="blue",
                     bg="red",
                     command=lambda odds=index[6]: calcOdds(odds),
+                    width=10
                 )
                 ml_home = Button(
                     home_frame,
@@ -109,8 +126,9 @@ def labels_buttons(root, csv_file):
                     fg="blue",
                     bg="red",
                     command=lambda odds=index[7]: calcOdds(odds),
+                    width=10
                 )
-                # Initialize Buttons
+                # Pack Buttons and add them to a list(widgets) to be deleted when changing weeks
                 away.pack(side="left")
                 widgets.append(away)
                 home.pack(side="left")
@@ -137,6 +155,9 @@ def labels_buttons(root, csv_file):
                 home_frame.pack()
                 widgets.append(away_frame)
                 widgets.append(home_frame)
-
+                # Space Frame to put a small gap between games
+                spaceFrame = Frame(root, height=10)
+                spaceFrame.pack()
+                widgets.append(spaceFrame)
 
 root.mainloop()
