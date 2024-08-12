4dd41842c5ae8af3f9fb403b8552e37297447739
jrsussner <jrsussner@gmail.com>
2024-08-11 22:01:11 -0500

    Update gui.py
    made it so all games are printed to GUI

--- gui.py
+++ gui.py
@@ -15,30 +15,33 @@ def labels_buttons(root, csv_file):
     with open(csv_file, "r", encoding="utf-8") as file:
         read = csv.reader(file)
         next(read)
-        for row, index in enumerate(read):
+        for x, index in enumerate(read):
             # Need help here, I have row set to 0 but this is where I would like to generate user input
-            if row == 0:
+            # if row == 0:
 
-                # Initialize Labels
-                away = Label(root, text=index[0])
-                home = Label(root, text=index[1])
-                spread_away = Button(root, text=index[2])
-                spread_odds_away = Button(root, text=index[3])
-                ml_away = Button(root, text=index[4])
-                spread_home = Button(root, text=index[5])
-                spread_odds_home = Button(root, text=index[6])
-                ml_home = Button(root, text=index[7])
+            # Initialize Labels
+            away = Label(root, text=index[0])
+            home = Label(root, text=index[1])
+            spread_away = Button(root, text=index[2])
+            spread_odds_away = Button(root, text=index[3])
+            ml_away = Button(root, text=index[4])
+            spread_home = Button(root, text=index[5])
+            spread_odds_home = Button(root, text=index[6])
+            ml_home = Button(root, text=index[7])
 
-                # Initialize Buttons
-                away.grid(row=1, column=0, sticky=W)
-                home.grid(row=2, column=0, sticky=W)
-                spread_away.grid(row=1, column=1)
-                spread_odds_away.grid(row=1, column=2)
-                ml_away.grid(row=1, column=3)
-                spread_home.grid(row=2, column=1)
-                spread_odds_home.grid(row=2, column=2)
-                ml_home.grid(row=2, column=3)
+            # Initialize Buttons
+            away.grid(row=(x * 2) + 1, column=0, sticky=W)
+            home.grid(row=(x * 2) + 2, column=0, sticky=W)
+            spread_away.grid(row=(x * 2) + 1, column=1)
+            spread_odds_away.grid(row=(x * 2) + 1, column=2)
+            ml_away.grid(row=(x * 2) + 1, column=3)
+            spread_home.grid(row=(x * 2) + 2, column=1)
+            spread_odds_home.grid(row=(x * 2) + 2, column=2)
+            ml_home.grid(row=(x * 2) + 2, column=3)
 
+
+scrollbar = Scrollbar(root)
+scrollbar.grid(row=0, column=4, rowspan=11, sticky="ns")
 
 # Initialize Labels
 question = Label(root, text="What week do you want to bet on?")
@@ -54,15 +57,15 @@ user_input = Entry(root, width=25)
 submit = Button(root, text="Submit", command=userInput)
 
 # Putting widgets in the GUI
-question.grid(row=10, column=0, sticky=W)
+# question.grid(row=10, column=0, sticky=W)
 teams_title.grid(row=0, column=0, sticky=W)
 spread_title.grid(row=0, column=1)
 spread_odds_title.grid(row=0, column=2)
 ml_title.grid(row=0, column=3)
 
-user_input.grid(row=10, column=2, sticky=W)
+# user_input.grid(row=10, column=2, sticky=W)
 
-submit.grid(row=10, column=3)
+# submit.grid(row=10, column=3)
 
 
 labels_buttons(root, "lines.csv")
